6c5546881384ddfbb880f66bbb8051e1467c91fe
Benjamin Eskildsen <eskildsen.ben@gmail.com>
2023-03-29 10:35:55 -0400

    modification script

--- script.js
+++ script.js
@@ -1,11 +1,70 @@
 const { exec } = require('child_process');
+const {
+  createConversation, addMessage,
+  submitConversation,
+} = require('bens_gpt');
+const fs = require('fs');
+const {gptAPIKey} = require('./.secrets');
 
-exec('cat script.js', (err, stdout, stderr) => {
-  if (err) {
-    console.error(`exec error: ${err}`);
-    return;
-  }
+// --------------------------------------------------------
+// Utils
+// --------------------------------------------------------
 
-  console.log(`stdout: ${stdout}`);
-  console.error(`stderr: ${stderr}`);
-});
+const asyncExec = (cmd) => {
+  return new Promise((resolve, reject) => {
+    exec(cmd, (err, stdout, stderr) => {
+      resolve({stdout, stderr, err});
+    });
+  });
+}
+
+// --------------------------------------------------------
+// Conversations
+// --------------------------------------------------------
+
+const grepConversation = createConversation(
+  {modelParams: {temperature: 0}},
+  [{role: 'system', content: 'You output regular expressions'}]
+);
+
+const createModificationConversation = (instructions) => {
+  return createConversation(
+    {modelParams: {temperature: 0}},
+    [{role: 'system', content: `
+      Modify the following file according to these instructions: ${instructions}
+      Don't make any changes to the file except those strictly required to follow the instructions
+      `
+    }]
+  );
+};
+
+// --------------------------------------------------------
+// Execution
+// --------------------------------------------------------
+
+const modifyFile = (fileName, instructions) => {
+  let modificationPrompt = createModificationConversation(instructions);
+  const fileContents = fs.readFileSync(fileName, 'utf-8');
+  modificationPrompt = addMessage(modificationPrompt, {role: 'user', content: fileContents});
+  console.log(modificationPrompt);
+
+  submitConversation(modificationPrompt, gptAPIKey)
+    .then(({message, tokens, finishReason}) => {
+      console.log(message);
+      fs.writeFileSync(fileName, message.content);
+    });
+}
+
+modifyFile(process.argv[2], process.argv[3]);
+
+// asyncExec('cat script.js')
+//   .then(({stdout}) => {
+//     return asyncExec(`echo '${stdout}' > foo`);
+//   })
+//   .then(({stdout, err, stderr}) => {
+//     if (err) console.error(err);
+//
+//     console.log(stdout);
+//     console.log(stderr);
+//   });
+
