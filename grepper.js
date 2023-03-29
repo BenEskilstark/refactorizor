d80a87239cee8ceeabea313424090269a41a0674
Benjamin Eskildsen <eskildsen.ben@gmail.com>
2023-03-29 15:23:37 -0400

    get it basically working

--- grepper.js
+++ grepper.js
@@ -3,7 +3,6 @@ const {
   createConversation, addMessage,
   submitConversation,
 } = require('bens_gpt');
-const fs = require('fs');
 const {gptAPIKey} = require('./.secrets');
 
 // --------------------------------------------------------
@@ -27,35 +26,9 @@ const grepConversation = createConversation(
   [{role: 'system', content: 'You output regular expressions'}]
 );
 
-const createModificationConversation = (instructions) => {
-  return createConversation(
-    {modelParams: {temperature: 0}},
-    [{role: 'system', content: `
-      Modify the following file according to these instructions: ${instructions}
-      Don't make any changes to the file except those strictly required to follow the instructions
-      `
-    }]
-  );
-};
-
 // --------------------------------------------------------
 // Execution
 // --------------------------------------------------------
-
-const modifyFile = (fileName, instructions) => {
-  let modificationPrompt = createModificationConversation(instructions);
-  const fileContents = fs.readFileSync(fileName, 'utf-8');
-  modificationPrompt = addMessage(modificationPrompt, {role: 'user', content: fileContents});
-  console.log(modificationPrompt);
-
-  submitConversation(modificationPrompt, gptAPIKey)
-    .then(({message, tokens, finishReason}) => {
-      console.log(message);
-      fs.writeFileSync(fileName, message.content);
-    });
-}
-
-modifyFile(process.argv[2], process.argv[3]);
 
 // asyncExec('cat script.js')
 //   .then(({stdout}) => {
